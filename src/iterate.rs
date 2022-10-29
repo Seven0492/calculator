434384f90f4993442b4f6ee7a47aa3dc6623d2ab
Seth Martin <sethm6348@gmail.com>
2022-10-28 22:16:24 -0400

    Added measure for going over initial text block

--- src/iterate.rs
+++ src/iterate.rs
@@ -16,52 +16,58 @@ pub fn run(args: &Vec<String>, i: &usize) {
         num2: String::new(),
     };
 
-    if i == &0 {
-        for n in args.iter() {
-            if n.parse::<f64>().is_ok() {
-                if search_for_symbol > 0 {
-                    doc::wrong_formating(1);
+    let mut passed_over_initial_block = 0;
 
-                    // Debugging
-                    println!("search_for_symbol: '{}'\n", search_for_symbol);
-                    // EOD
+    for n in args.iter() {
+        if i > &0 || &passed_over_initial_block != i {
+            passed_over_initial_block += 1;
 
-                    doc::help();
+            continue;
+        }
 
-                    return;
-                } else {
-                    search_for_symbol = 1;
+        if n.parse::<f64>().is_ok() {
+            if search_for_symbol > 0 {
+                doc::wrong_formating(1);
 
-                    if set.num1 == "" {
-                        set.num1 = n.clone();
-                    } else if set.num2 == "" {
-                        set.num2 = n.clone();
-                    } else {
-                        set.num1 = n.clone();
-                        let _ = set.num2 == String::new();
-                    }
+                // Debugging
+                println!("search_for_symbol: '{}'\n", search_for_symbol);
+                // EOD
 
-                    continue;
-                }
+                doc::help();
+
+                return;
             } else {
-                if search_for_symbol == 1 {
-                    module::operations(
-                        &vec![set.num1.clone(), set.symbol.clone(), set.num2.clone()],
-                        &0,
-                    );
+                search_for_symbol = 1;
 
-                    continue;
+                if set.num1 == "" {
+                    set.num1 = n.clone();
+                } else if set.num2 == "" {
+                    set.num2 = n.clone();
                 } else {
-                    doc::wrong_formating(1);
-
-                    // Debugging
-                    println!("search_for_symbol: '{}'\n", search_for_symbol);
-                    // EOD
-
-                    doc::help();
-
-                    return;
+                    set.num1 = n.clone();
+                    let _ = set.num2 == String::new();
                 }
+
+                continue;
+            }
+        } else {
+            if search_for_symbol == 1 {
+                module::operations(
+                    &vec![set.num1.clone(), set.symbol.clone(), set.num2.clone()],
+                    &0,
+                );
+
+                continue;
+            } else {
+                doc::wrong_formating(1);
+
+                // Debugging
+                println!("search_for_symbol: '{}'\n", search_for_symbol);
+                // EOD
+
+                doc::help();
+
+                return;
             }
         }
     }
