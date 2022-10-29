e4900744f370fe1376b165faf413c5a4fe4e49d2
Seth Martin <88vcstbtf@mozmail.com>
2022-10-28 22:51:04 -0400

    Fix indentation

--- src/doc/mod.rs
+++ src/doc/mod.rs
@@ -3,12 +3,12 @@ pub fn help() {
         "Pass in a single basic mathematical operation to be made.
 
 Options:
-Addition `2 + 4`,
-Substract ` 2 - 4`,
-Multiplication `2 x 4` but using '*' without quotes can create problems,
-Division `2 / 4`.
+  Addition `2 + 4`,
+  Substract ` 2 - 4`,
+  Multiplication `2 x 4` but using '*' without quotes can create problems,
+  Division `2 / 4`.
 
-Btw, If you just type two numbers separated by spaces without any symbols,
+If you just type two numbers separated by spaces without any symbols,
 it will default to multiplication.
 
 For example: `./executable.exe 1.2 + 63.52` or `./executable.exe 2 x 4`\n"
